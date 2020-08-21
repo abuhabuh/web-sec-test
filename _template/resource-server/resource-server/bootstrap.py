69ace6bda7014a24c654bd5291a578c7c245c023
John Wang <jue@alumni.cmu.edu>
2020-08-20 23:25:47 -0400

    template: complete

--- _template/resource-server/resource-server/bootstrap.py
+++ _template/resource-server/resource-server/bootstrap.py
@@ -1,4 +1,5 @@
 import flask
+from flask_cors import CORS, cross_origin
 
 from apis import base_api
 
@@ -10,3 +11,33 @@ def bootstrap_app(jinja_env):
 
     return flask_app
 
+
+def add_cors(app):
+    """
+    DEPRECATED
+
+    The CORS spec here dictates what requests are processed from 
+    another origin.
+
+    * POST /name via default Content-Type (or any Content-Type that 
+      does not trigger a preflight request) is always processed 
+      regardless of CORS being active or the method specified in 
+      the CORS `resources` section. The response (without CORS 
+      auth) will be dropped by the web client, but the POST command 
+      will have been executed by this server.
+
+    * GET /name 
+      * Will be processed by this server regardless of CORS because 
+        it is a "simple request".
+      * The web client will drop the response if CORS headers are 
+        not specified by this server via the flask_cors.CORS config
+
+    """
+    CORS(app,
+        resources={
+            '/name': {
+                'origins': ['http://localhost:5001'], 
+                'methods': ['GET', 'POST'],
+            }
+        },
+    )
